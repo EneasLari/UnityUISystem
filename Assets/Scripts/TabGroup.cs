2c453a310316d7c50804a0a360cbe829aa54ad1a
EneasLari <cs02367@cc.uoi.gr>
2020-10-30 03:04:20 +0200

    tab system finished

---
+++ Assets/Scripts/TabGroup.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TabGroup : MonoBehaviour
+{
+    public List<TabButton> tabButtons;
+
+    //if we use sprites for different button states
+    public Sprite tabIdle;
+    public Sprite tabHover;
+    public Sprite tabActive;
+
+    //if we use colors for different button states
+    public Color ctabIdle;
+    public Color ctabHover;
+    public Color ctabActive;
+
+    public TabButton selectedTab;
+
+    //pages to swap through tabs
+    public List<GameObject> objectsToSwap;
+
+    public void Subscribe(TabButton button) {
+        if (tabButtons == null) {
+            tabButtons = new List<TabButton>();
+        }
+        tabButtons.Add(button);
+    }
+
+    public void OnTabEnter(TabButton button) {
+        ResetTabs();
+        if (selectedTab == null && button != selectedTab) {
+            button.background.sprite = tabHover;
+            button.background.color = ctabHover;
+        }
+
+    }
+
+    public void OnTabExit(TabButton button) {
+        ResetTabs();
+    }
+
+    public void OnTabSelected(TabButton button) {
+        if (selectedTab!=null) {
+            selectedTab.Deselect();
+        }
+
+        selectedTab = button;
+
+        selectedTab.Select();
+
+        ResetTabs();
+        button.background.sprite = tabActive;
+        button.background.color = ctabActive;
+        //tabs must have same index with pages
+        int index = button.transform.GetSiblingIndex();
+        for (int i = 0; i < objectsToSwap.Count; i++) {
+            if (i == index) {
+                objectsToSwap[i].SetActive(true);
+            } else {
+                objectsToSwap[i].SetActive(false);
+            }
+        }
+    }
+
+    public void ResetTabs() {
+        foreach (TabButton button in tabButtons) {
+            if (selectedTab != null && button == selectedTab) {
+                continue;
+            }
+            button.background.sprite = tabIdle;
+            button.background.color = ctabIdle;
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -3,3 +3,7 @@ UI system based on layout and grids
 
 This project is a general UI system based on tutorials by this channel:
 [Game Dev Guide](https://www.youtube.com/channel/UCR35rzd4LLomtQout93gi0w)
+
+### Videos Explaining these:
+[Creating a Custom Tab System in Unity](https://www.youtube.com/watch?v=211t6r12XPQ&ab_channel=GameDevGuide)
+[Making UI That Looks Good In Unity using Color Palettes, Layout Components and a Blur Panel](https://www.youtube.com/watch?v=HwdweCX5aMI&t=711s&ab_channel=GameDevGuide)
